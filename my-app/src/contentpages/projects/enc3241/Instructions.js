15ce9bb8e3e18ecf40a8a104546b8cce06fb7eb4
Mark Williams <markie0317@gmail.com>
2022-11-27 22:57:29 -0500

    in update

--- my-app/src/contentpages/projects/enc3241/Instructions.js
+++ my-app/src/contentpages/projects/enc3241/Instructions.js
@@ -3,7 +3,6 @@ import instructions from './src/MinecraftInstructionsFinal.pdf';
 
 import Project from '../Project';
 import ListItem from '../../ListItem';
-import Callout from '../../../tools/Callout';
 
 function Instructions() {
     return ( 
@@ -14,8 +13,9 @@ function Instructions() {
                 </h1>
                 Below is instructions for building a "Jeb" door within Minecraft. I wrote it inside ENC 3241 and it challenged me to design instructions that interested me. For this I chose Minecraft as it was a childhood video game that I would play for hours. This was one of the first redstone designs I learned and I wanted to move forward the knowledge.
                 <iframe src={instructions} title="Instructions" width="100%" height="500px"></iframe>
-                <div className='fig-text'><ListItem isFile href={instructions} title="Instructions Link" desc="" /></div>
-                <Callout>NOTE: The PDF above is an unrevised version of my instructions.</Callout>
+                <div className='fig-text'><ListItem isFile href={instructions} title="Instructions Link" desc={<p className='fig-text'>
+                    *Not up to date and unrevised.
+                </p>} /></div>
             </div>
         </Project>
     );
