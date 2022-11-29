72f6d82173ba009379536673e64fefd01a3a90b2
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2022-11-29 00:43:18 -0500

    Included my resume and cover letter

--- my-app/src/contentpages/projects/enc3241/JobMaterials.js
+++ my-app/src/contentpages/projects/enc3241/JobMaterials.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import rc from './src/ResumeAndCover.pdf';
 import Project from '../Project';
 
 function JobMaterials() {
@@ -9,6 +10,16 @@ function JobMaterials() {
                 <h1>
                     Job Materials
                 </h1>
+                <h2>
+                    Unrevised Job Materials
+                </h2>
+                <p>
+                    Below are my unrevised (current) job materials, including my resume and cover letter.
+                </p>
+                <iframe src={rc} title="Instructions" width="100%" height="500px"></iframe>
+                <h3>
+                    Video
+                </h3>
             </div>
         </Project>
     );
