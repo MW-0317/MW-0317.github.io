e7006f9388219c653f718776836d55a38aa57c9a
Mark Williams <mark.williams1703@gmail.com>
2023-09-22 14:49:11 -0400

    Projects List Items to projects as card items

---
+++ my-app/src/contentpages/CardItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CardItem.css';
+
+function CardItem( {href, title, desc, isFile, imgSrc} ) {
+    return ( 
+        <div className='card-resource'>
+            <div className='card-image'>
+                <img src={imgSrc} alt="N/A"/>
+            </div>
+            <div className='card-text'>
+                {isFile ? <a className='link' href={href}> {title} </a> : <Link className='link' to={href}> {title} </Link>}
+                <div className='card-detail'>
+                    {desc}
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default CardItem;
--- my-app/src/contentpages/Projects.js
+++ my-app/src/contentpages/Projects.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Content from '../Content';
 import ListItem from './ListItem';
+import FlexList from './FlexList';
+import CardItem from './CardItem';
+
+import luna from "./resourcesrc/drawing.png";
 
 function Projects() {
     return ( 
@@ -11,7 +15,12 @@ function Projects() {
                 </h1>
                 {/* <ListItem href="/project/example"   title="Example Page" desc="Example Page for a project"/> */}
                 {/* <ListItem href="/project/u1v1"      title="Untitled1v1 Game Resources" desc="For those involved in creation of the game"/> */}
-                <ListItem href="/project/luna"                  title="Luna Engine"             desc="A game or rendering engine named luna."/>
+                <FlexList>
+                    <CardItem href="/project/luna"                  title="Luna Engine"             
+                    desc="A game or rendering engine named luna."
+                    imgSrc={luna}/>
+                </FlexList>
+                
                 <h1>
                     Projects @ UCF
                 </h1>
@@ -22,9 +31,14 @@ function Projects() {
                 <h2>
                     ENC 3241
                 </h2>
-                <ListItem href="/project/enc3241/instructions"  title="Minecraft Instructions"  desc="The instructions for building a basic Minecraft Redstone Build, specifically the 'Jeb' door. See instructions for defintion." />
-                <ListItem href="/project/enc3241/jobmat"        title="Job Materials"           desc="My current job materials for applying to a job, including my resume, cover letter, and video interview." />
-                <ListItem href="/project/enc3241/research"      title="Research Proposal"       desc="A mock research proposal for an Undergraduate Research opportunity. This proposal specialized in the concept of COVID-19 in the classroom and it's effect on stress in close proximity to one another." />
+                <FlexList>
+                    <CardItem href="/project/enc3241/instructions"  title="Minecraft Instructions"  
+                        desc="The instructions for building a basic Minecraft Redstone Build, specifically the 'Jeb' door. See instructions for defintion." 
+                        imgSrc=""/>
+                    <CardItem href="/project/enc3241/jobmat"        title="Job Materials"           desc="My current job materials for applying to a job, including my resume, cover letter, and video interview." />
+                    <CardItem href="/project/enc3241/research"      title="Research Proposal"       desc="A mock research proposal for an Undergraduate Research opportunity. This proposal specialized in the concept of COVID-19 in the classroom and it's effect on stress in close proximity to one another." />
+                </FlexList>
+                
             </div>
         </Content>
      );
