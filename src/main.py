dd66cd7bf337d302112329113fe80d802615d3f7
MW-0317 <mark.williams1703@gmail.com>
2026-01-28 01:01:15 -0500

    Cleaned the code up a bit

--- src/main.py
+++ src/main.py
@@ -10,76 +10,77 @@ def remap_links(doc):
             elm[2] = (os.path.splitext(elm[2][0])[0] + ".html", '')
 
 class ObsidianVault:
-    vault_path: Path
-    fmt: str
-    def __init__(self, vault_path: Path, fmt: str):
-        self.vault_path = vault_path
+    vault_path : Path
+    header : Path
+    templates : Path
+    temp : Path
+    fmt : str
+
+    def __init__(self, fmt: str):
         self.fmt = fmt
 
-    def to(self, folder: Path, ext: str):
-        shutil.copytree("./src/header", folder / "header", dirs_exist_ok=True)
-        for path, dirs, files in os.walk(self.vault_path):
-            p = Path(path)
-            if "obsidian" in path:
-                continue
-            for dir in dirs:
-                d = Path(dir)
-                main = p.relative_to("main")
-                new_dir = folder / main / d
-                if not os.path.isdir(new_dir):
-                    print("Making Dir:", new_dir)
-                    new_dir.mkdir()
-            for file in files:
-                f = Path(file)
-                read_file = p / f
+    def to(self, folder: Path, extension: str):
+        shutil.copytree(self.header, folder / "header", dirs_exist_ok=True)
 
-                main = read_file.relative_to("main")
-                if not file.endswith("md"):
-                    shutil.copy2(read_file, folder / main)
+        for path, dirs, files in os.walk(self.vault_path):
+            if ".obsidian" in path:
+                continue
+
+            path = Path(path)
+            for dir in dirs:
+                directory = Path(dir)
+                relative_to_main = path.relative_to("main")
+                new_directory = folder / relative_to_main / directory
+                if not os.path.isdir(new_directory):
+                    print("Making Dir:", new_directory)
+                    new_directory.mkdir()
+
+            for file in files:
+                file_string = file
+                file = Path(file)
+                read_file = path / file
+                relative_to_main = read_file.relative_to("main")
+
+                if not file_string.endswith("md"):
+                    shutil.copy2(read_file, folder / relative_to_main)
                     continue
+
                 
                 print("Reading:", read_file)
                 doc = pandoc.read(file=str(read_file), format=self.fmt)
-                remap_links(doc)
-                
-                write_file = folder / main.with_suffix('.%s'% ext)
 
+                # Convert .md links to .html
+                remap_links(doc)
+
+                print("Getting templates...")
+                header_template = ""
+                with open(self.templates / "header.html", "r") as header_template_file:
+                    header_template = "".join(header_template_file.readlines())
+
+                sidebar_template = ""
+                with open(self.templates / "sidebar.html", "r") as sidebar_template_file:
+                    sidebar_template = "".join(sidebar_template_file.readlines())
+
+                write_file = folder / relative_to_main.with_suffix('.%s'% extension)
                 print("Writing:", write_file)
-                wiki = Path("./src/wiki.html")
-                with open("./src/temp_header.html", "w") as temp_header:
-                    temp = '<script src="{0}header/script.js"></script> <link rel="stylesheet" href="{0}header/styles.css">'.format("../" * (len(write_file.parents) - 2) + "./")
+
+                with open(self.temp / "temp_header.html", "w") as temp_header:
+                    temp = header_template.format("../" * (len(write_file.parents) - 2) + "./")
                     temp_header.write(temp)
-                with open("./src/temp_sidebar.html", "w") as temp_side_bar:
-                    # <canvas height="256" width="512" style="margin: 0 auto; width: 50%; display: block;" id="gl-canvas"></canvas>
-                    temp_s = """
-<nav id="TON">
-    <h1>Mark Williams</h1>
-    Aspiring Graphics Programmer
-    <div style="display: flex;">
-        <ul>
-            <li>
-                <a href="{0}Main.html">Home</a>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <a href="{0}Articles.html">Articles</a>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <a href="{0}Projects.html">Projects</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-""".format("../" * (len(write_file.parents) - 2) + "./")
-                    temp_side_bar.write(temp_s)
-                header = Path("./src/temp_header.html")
-                sidebar = Path("./src/temp_sidebar.html")
+
+                with open(self.temp / "temp_sidebar.html", "w") as temp_sidebar:
+                    temp_s = sidebar_template.format("../" * (len(write_file.parents) - 2) + "./")
+                    temp_sidebar.write(temp_s)
+
+                header = Path(self.temp / "temp_header.html")
+                sidebar = Path(self.temp / "temp_sidebar.html")
                 end = Path("./src/end.html")
                 pandoc.write(doc, options=["--toc", "--standalone", "-c", ".", "-H", header, "-B", sidebar, "-A", end], file=str(write_file))
 
 if __name__ == "__main__":
-    ov = ObsidianVault(Path("main"), "gfm")
+    ov = ObsidianVault("gfm")
+    ov.vault_path = Path("main")
+    ov.header = Path("src/header")
+    ov.templates = Path("src/html")
+    ov.temp = Path("src/temp_html")
     ov.to(Path("build"), "html")
